c156cd6723eaed9fa898cb4fded4f77012d6b9a1
Chakrasai <chakrasaiaku@gmail.com>
2025-02-15 18:44:24 +0530

    initialize React app with basic structure and components

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+// import {Menu,X,Moon,Sun} from "lucide-react";
+function Navigation() {
+  return (
+    <div>
+      <section className='nav-section'>
+            <nav>
+                <div className='nav-title'>
+                  Chakrasai Akuthota
+                </div>
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </nav>
+      </section>
+    </div>
+  )
+}
+
+export default Navigation
